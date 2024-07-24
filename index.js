611c773aacc7eed342c3cc85416c57505b2096b2
anitah254-pixel <ann.kilonzo@student.moringaschool.com>
2024-07-24 10:25:13 +0300

    Refactor distance calculation functions and fare pricing logic in index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  const headquarters = 42;
+  return Math.abs(headquarters - someValue);
+}
+
+function distanceFromHqInFeet(someValue) {
+  const distanceInBlocks = distanceFromHqInBlocks(someValue);
+  return distanceInBlocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs((destination - start) * 264);
+}
+
+function calculatesFarePrice(start, destination) {
+  let distanceInFeet = distanceTravelledInFeet(start, destination); // ?
+  if (distanceInFeet <= 400) {
+    return 0;
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    distanceInFeet -= 400;
+    const cents = distanceInFeet * 2;
+    const dollars = cents / 100;
+    return dollars;
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
